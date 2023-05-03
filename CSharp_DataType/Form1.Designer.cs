dac954614aa970e4735e2a1367b9d3b8a3002194
vkeh9656 <37315637+vkeh9656@users.noreply.github.com>
2023-05-03 21:41:01 +0900

    초기 UI 및 name 세팅

--- CSharp_DataType/Form1.Designer.cs
+++ CSharp_DataType/Form1.Designer.cs
@@ -28,77 +28,154 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.tboxNumber = new System.Windows.Forms.TextBox();
+            this.btnExe = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.lblShort = new System.Windows.Forms.Label();
+            this.lblInt = new System.Windows.Forms.Label();
+            this.lblDouble = new System.Windows.Forms.Label();
+            this.lblException = new System.Windows.Forms.Label();
+            this.btnShort = new System.Windows.Forms.Button();
+            this.btnInt = new System.Windows.Forms.Button();
+            this.btnDouble = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // textBox1
+            // tboxNumber
             // 
-            this.textBox1.Location = new System.Drawing.Point(393, 63);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 21);
-            this.textBox1.TabIndex = 0;
+            this.tboxNumber.Location = new System.Drawing.Point(12, 12);
+            this.tboxNumber.Name = "tboxNumber";
+            this.tboxNumber.Size = new System.Drawing.Size(639, 21);
+            this.tboxNumber.TabIndex = 0;
             // 
-            // button1
+            // btnExe
             // 
-            this.button1.Location = new System.Drawing.Point(550, 60);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnExe.Location = new System.Drawing.Point(670, 10);
+            this.btnExe.Name = "btnExe";
+            this.btnExe.Size = new System.Drawing.Size(106, 23);
+            this.btnExe.TabIndex = 1;
+            this.btnExe.Text = "알아서 변환";
+            this.btnExe.UseVisualStyleBackColor = true;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(33, 90);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.Size = new System.Drawing.Size(34, 12);
             this.label1.TabIndex = 2;
-            this.label1.Text = "label1";
+            this.label1.Text = "Short";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(33, 136);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 12);
+            this.label2.Size = new System.Drawing.Size(18, 12);
             this.label2.TabIndex = 3;
-            this.label2.Text = "label2";
+            this.label2.Text = "Int";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(33, 177);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(38, 12);
+            this.label3.Size = new System.Drawing.Size(44, 12);
             this.label3.TabIndex = 4;
-            this.label3.Text = "label3";
+            this.label3.Text = "Double";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(33, 221);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(38, 12);
+            this.label4.Size = new System.Drawing.Size(61, 12);
             this.label4.TabIndex = 5;
-            this.label4.Text = "label4";
+            this.label4.Text = "Exception";
+            // 
+            // lblShort
+            // 
+            this.lblShort.AutoSize = true;
+            this.lblShort.Location = new System.Drawing.Point(136, 89);
+            this.lblShort.Name = "lblShort";
+            this.lblShort.Size = new System.Drawing.Size(11, 12);
+            this.lblShort.TabIndex = 6;
+            this.lblShort.Text = "0";
+            // 
+            // lblInt
+            // 
+            this.lblInt.AutoSize = true;
+            this.lblInt.Location = new System.Drawing.Point(136, 136);
+            this.lblInt.Name = "lblInt";
+            this.lblInt.Size = new System.Drawing.Size(11, 12);
+            this.lblInt.TabIndex = 7;
+            this.lblInt.Text = "0";
+            // 
+            // lblDouble
+            // 
+            this.lblDouble.AutoSize = true;
+            this.lblDouble.Location = new System.Drawing.Point(136, 177);
+            this.lblDouble.Name = "lblDouble";
+            this.lblDouble.Size = new System.Drawing.Size(11, 12);
+            this.lblDouble.TabIndex = 8;
+            this.lblDouble.Text = "0";
+            // 
+            // lblException
+            // 
+            this.lblException.AutoSize = true;
+            this.lblException.Location = new System.Drawing.Point(136, 221);
+            this.lblException.Name = "lblException";
+            this.lblException.Size = new System.Drawing.Size(11, 12);
+            this.lblException.TabIndex = 9;
+            this.lblException.Text = "-";
+            // 
+            // btnShort
+            // 
+            this.btnShort.Location = new System.Drawing.Point(354, 77);
+            this.btnShort.Name = "btnShort";
+            this.btnShort.Size = new System.Drawing.Size(96, 23);
+            this.btnShort.TabIndex = 10;
+            this.btnShort.Text = "Short 변환";
+            this.btnShort.UseVisualStyleBackColor = true;
+            // 
+            // btnInt
+            // 
+            this.btnInt.Location = new System.Drawing.Point(354, 131);
+            this.btnInt.Name = "btnInt";
+            this.btnInt.Size = new System.Drawing.Size(96, 23);
+            this.btnInt.TabIndex = 11;
+            this.btnInt.Text = "Int 변환";
+            this.btnInt.UseVisualStyleBackColor = true;
+            // 
+            // btnDouble
+            // 
+            this.btnDouble.Location = new System.Drawing.Point(354, 177);
+            this.btnDouble.Name = "btnDouble";
+            this.btnDouble.Size = new System.Drawing.Size(96, 23);
+            this.btnDouble.TabIndex = 12;
+            this.btnDouble.Text = "Double 변환";
+            this.btnDouble.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnDouble);
+            this.Controls.Add(this.btnInt);
+            this.Controls.Add(this.btnShort);
+            this.Controls.Add(this.lblException);
+            this.Controls.Add(this.lblDouble);
+            this.Controls.Add(this.lblInt);
+            this.Controls.Add(this.lblShort);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnExe);
+            this.Controls.Add(this.tboxNumber);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -108,12 +185,19 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tboxNumber;
+        private System.Windows.Forms.Button btnExe;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblShort;
+        private System.Windows.Forms.Label lblInt;
+        private System.Windows.Forms.Label lblDouble;
+        private System.Windows.Forms.Label lblException;
+        private System.Windows.Forms.Button btnShort;
+        private System.Windows.Forms.Button btnInt;
+        private System.Windows.Forms.Button btnDouble;
     }
 }
 
